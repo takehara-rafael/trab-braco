21b620d9c95d510d4ec5fa616cd8ad11934c68f8
Takehara <75047337+takehara-rafael@users.noreply.github.com>
2021-12-05 00:34:05 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <GL/freeglut.h>
 
+#define WINDOW_SIZE 700
+
 int proj=0;
 
-int posX=0, posY=5, posZ=10;
+int posX=0, posY=10, posZ=15;
 int oX=0, oY=0, oZ=0;
 int lX=0, lY=1, lZ=0;
 
@@ -14,6 +16,9 @@ void Display();
 void Mouse(int btn, int state, int x, int y);
 void Keyboard(unsigned char key, int x, int y);
 void SpecKeys(int key, int x, int y);
+void BuildScene();
+void BuildArm();
+
 
 /******************************************************************/
 
@@ -25,28 +30,94 @@ void Display() {
     glEnable(GL_BLEND);
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
-    
+
     glLoadIdentity();
     if(proj==1) {
         glOrtho(-50, 50, -50, 50, -50, 50);
     } else {
         gluPerspective(45,1,1,50);
     }
-    
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    
+
     gluLookAt(posX, posY, posZ, oX, oY, oZ, lX, lY, lZ);
-    
+
     glRotatef(rot, lX, lY, lZ);
-    
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    
-   glColor3ub(255, 255, 255);
-   glutWireCube(1.0);
-   
-   glutSwapBuffers();
+
+    glPushMatrix();
+    BuildScene();
+    glPopMatrix();
+
+    glPushMatrix();
+    BuildArm();
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+/******************************************************************/
+
+void BuildScene() {
+    glPushMatrix();
+    glColor3ub(255, 255, 255);
+    glScalef(1, 0.01, 1);
+    glutSolidCube(10.0);
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glColor3ub(150, 75, 0);
+
+    glPushMatrix();
+    glTranslatef(0, 2, 0);
+    glScalef(1, 0.02, 1);
+    glutSolidCube(5.0);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(2, 0, 2);
+    glRotatef(90, -1, 0, 0);
+    glScalef(0.1, 0.1, 1);
+    glutSolidCylinder(1, 2, 50, 1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-2, 0, 2);
+    glRotatef(90, -1, 0, 0);
+    glScalef(0.1, 0.1, 1);
+    glutSolidCylinder(1, 2, 50, 1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-2, 0, -2);
+    glRotatef(90, -1, 0, 0);
+    glScalef(0.1, 0.1, 1);
+    glutSolidCylinder(1, 2, 50, 1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(2, 0, -2);
+    glRotatef(90, -1, 0, 0);
+    glScalef(0.1, 0.1, 1);
+    glutSolidCylinder(1, 2, 50, 1);
+    glPopMatrix();
+
+    glPopMatrix();
+}
+
+/******************************************************************/
+
+void BuildArm() {
+
+    glPushMatrix();
+    glColor3ub(51, 51, 255);
+    glTranslatef(0, 2.5, 0);
+    glutSolidCube(1.0);
+    glPopMatrix();
+
 }
 
 /******************************************************************/
@@ -109,6 +180,12 @@ void Keyboard(unsigned char key, int x, int y) {
         rot-=5;
     } else if(key==27) {
         exit(0);
+    } else if(key=='p') {
+        proj=0;
+        posX=0, posY=10, posZ=15;
+        oX=0, oY=0, oZ=0;
+        lX=0, lY=1, lZ=0;
+        rot=0;
     }
     glutPostRedisplay();
 }
@@ -143,19 +220,18 @@ void SpecKeys(int key, int x, int y) {
 int main(int argc, char **argv) {
 
     glutInit(&argc, argv);
-    
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(10, 10);
+
+    glutInitWindowSize(WINDOW_SIZE, WINDOW_SIZE);
+    glutInitWindowPosition(((glutGet(GLUT_SCREEN_WIDTH)-WINDOW_SIZE)/2), ((glutGet(GLUT_SCREEN_HEIGHT))-WINDOW_SIZE)/2);
     glutCreateWindow("Braco");
-    
+
     glutDisplayFunc(Display);
     glutMouseFunc(Mouse);
     glutKeyboardFunc(Keyboard);
     glutSpecialFunc(SpecKeys);
-    
+
     glutMainLoop();
 
     return 0;
-    }
+}
