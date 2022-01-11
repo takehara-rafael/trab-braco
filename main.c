75dca3b150955cf49e5ea5dae7e4a6fb809df672
Gabrielrucker <gabrielrucker@hotmail.com>
2022-01-11 19:13:23 -0300

    correção da geração aleatória do cubo

--- main.c
+++ main.c
@@ -6,23 +6,20 @@
 
 #define WINDOW_SIZE 700
 
-struct parte {
-    int ang_x, ang_y, ang_z;
-    double pos_x, pos_y, pos_z;
-} dedo_esq, dedo_dir, garra, cubo;
-
 int proj=0;
 int aux=1, i=0;
-float posX=25, posY=15+2*sin(25*(3.14/4.0)), posZ=sqrt(pow(25,2)-pow(25,2)),y=0,z=0;
-int oX=3.2, oY=2.5, oZ=0;
-int lX=0, lY=1, lZ=0;
+float Xpos=25, Ypos=15+2*sin(25*(M_PI_4)), Zpos=sqrt(pow(25,2)-pow(25,2)),y=0,z=0;
+int Xo=3.2, Yo=2.5, Zo=0;
+int Xl=0, Yl=1, Zl=0;
 float vertices[150];
 int rot=0;
 
-int angulo1 = 0, angulo2 = 0, angulo3 = 0, angulo4 = 0, angulo5 = 0, angulo6 = 0,
-angulo7 = 0, angulo8 = 0, angulo9 = 0;
+int angle1 = 0, angle2 = 0, angle3 = 0, angle4 = 0, angle5 = 0, angle6 = 0,
+angle7 = 0, angle8 = 0, angle9 = 0;
 
-int grab = 0;
+float pos1=0, pos2=0, pos3=0, pos4=0, pos5=0, pos6=0;
+
+int pause, choice=0;
 
 double rand_x = 0, rand_z = 0;
 
@@ -32,8 +29,8 @@ void Keyboard(unsigned char key, int x, int y);
 void SpecKeys(int key, int x, int y);
 void BuildScene();
 void BuildArm();
-void Desenhocaminho();
-void DrawCube(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength);
+void DrawPath();
+void DrawCube(GLfloat centerXpos, GLfloat centerYpos, GLfloat centerZpos, GLfloat edgeLength);
 void FollowCurve(int);
 double rand_number();
 
@@ -59,21 +56,14 @@ void Display() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    gluLookAt(posX, posY, posZ, oX, oY, oZ, lX, lY, lZ);
+    gluLookAt(Xpos, Ypos, Zpos, Xo, Yo, Zo, Xl, Yl, Zl);
 
-    glRotatef(rot, lX, lY, lZ);
+    glRotatef(rot, Xl, Yl, Zl);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glPushMatrix();
-        glColor3ub(255,0,255);
-        if(!grab)
-            DrawCube(rand_x, 2.6, rand_z, 0.5);
-        glPopMatrix();
-
-    glPushMatrix();
-        Desenhocaminho();
-    glPopMatrix();
+    glColor3ub(255,0,255);
+    DrawCube(rand_x, 2.6, rand_z, 0.5);
 
     glPushMatrix();
         BuildScene();
@@ -83,20 +73,23 @@ void Display() {
         BuildArm();
     glPopMatrix();
 
+    glPushMatrix();
+        glColor3f(1,0,0);
+        DrawPath();
+    glPopMatrix();
+
     glutSwapBuffers();
 }
 
 /******************************************************************/
 
 void BuildScene() {
-
-    glPushMatrix();
     
     //chão
     glPushMatrix();
         glColor3ub(255, 255, 255);
         glScalef(1, 0.0001, 1);
-        glutSolidCube(100.0);
+        glutSolidCube(50.0);
     glPopMatrix();
 
     //mesa
@@ -131,15 +124,12 @@ void BuildScene() {
         glScalef(0.5, 2, 0.5);
         glutSolidCube(1.0);
     glPopMatrix();
-
-    
-    glPopMatrix();
 }
 
 /********************************************************************************/
 
 
-void Desenhocaminho(){
+void DrawPath(){
 
     //desenho do caminho da camera
 
@@ -180,21 +170,20 @@ void Desenhocaminho(){
 
 void BuildArm() {
 
-    glPushMatrix();
-
     //primeiro elo
-    glColor3ub(51, 51, 255);
-    glTranslatef(0, 2.5, 0);
-    glutSolidCube(1.0);
+        glColor3ub(51, 51, 255);
+        glTranslatef(0, 2.5, 0);
+        glutSolidCube(1.0);
 
     //primeira esfera
     glColor3f(1,0,0);
-    glTranslatef(0,1,0);
+    glTranslatef(0,0.6,0);
     glScalef(0.6,0.6,0.6);
-    glRotatef(angulo1, 1, 0, 0);
-    glRotatef(angulo2, 0, 1, 0);
-    glRotatef(angulo3, 0, 0, 1);
+    glRotatef(angle1, 1, 0, 0);
+    glRotatef(angle2, 0, 1, 0);
+    glRotatef(angle3, 0, 0, 1);
     glutSolidSphere(0.7,20,20);
+
 
     //segundo elo
     glColor3f(0,1,0);
@@ -207,9 +196,9 @@ void BuildArm() {
     //segunda esfera
     glColor3f(1,0,0);
     glTranslatef(0,2,0);
-    glRotatef(angulo4, 1, 0, 0);
-    glRotatef(angulo5, 0, 1, 0);
-    glRotatef(angulo6, 0, 0, 1);
+    glRotatef(angle4, 1, 0, 0);
+    glRotatef(angle5, 0, 1, 0);
+    glRotatef(angle6, 0, 0, 1);
     glutSolidSphere(0.7,20,20);
 
     //terceiro elo
@@ -223,39 +212,27 @@ void BuildArm() {
     //terceira esfera
     glColor3f(1,0,0);
     glTranslatef(2,0,0);
-    glRotatef(garra.ang_x, 1, 0, 0);
-    glRotatef(garra.ang_y, 0, 1, 0);
-    glRotatef(garra.ang_z, 0, 0, 1);
-    glutSolidSphere(0.7,20,20);
-
-    //garra
-    glColor3f(0,1,0);
-    glTranslatef(0.85,0,0); 
-    glScalef(1,1,5);
-    glutSolidCube(0.5);
-
-    if(abs(garra.pos_x - rand_x) < 0.5 && abs(garra.pos_y - 2.6) < 0.5 && abs(garra.pos_z - rand_z) < 0.5) {
-        grab = 1;
-        DrawCube(0.2,0,0, 0.5);
-    }
-
-
-    glPushMatrix(); 
+        glRotatef(angle7, 1, 0, 0);
+        glRotatef(angle8, 0, 1, 0);
+        glRotatef(angle9, 0, 0, 1);
+        glutSolidSphere(0.7,20,20);
         glColor3f(0,1,0);
-        glTranslatef(0,0,dedo_esq.pos_z);
-        glTranslatef(0.75,0,-0.1);
-        glScalef(5,0.5,0.5);
-        glutSolidCube(0.25);
-    glPopMatrix();
-    glPushMatrix();
-        glColor3f(0,1,0);
-        glTranslatef(0,0,dedo_dir.pos_z);
+        glTranslatef(0.85,0,0);
+        glScalef(1,1,5);
+        glutSolidCube(0.5);
+        
+        glPushMatrix();
+            glTranslatef(0,0, pos3);
+            glTranslatef(0.75,0,-0.1);
+            glScalef(5,0.5,0.5);
+            glutSolidCube(0.25);
+        glPopMatrix();
+        
+        glTranslatef(0,0, pos6);
         glTranslatef(0.75,0, 0.1);
         glScalef(5,0.5,0.5);
         glutSolidCube(0.25);
-    glPopMatrix();
-
-    glPopMatrix();
+        glPopMatrix();
 
 }
 
@@ -263,17 +240,18 @@ void BuildArm() {
 
 double rand_number(double min, double max) {
 
-    int choice;
-    double num;
+    double num=0;
 
     choice = rand() % 2; // 0 ou 1
-    if(choice)
-    {
-        num = (((double)rand()/(double)RAND_MAX)*min); //gera número entre min e 0
-    }
-    else
-    {
-        num = (((double)rand()/(double)RAND_MAX)*max); // gera número entre 0 e max
+    while(num>(min/3) && num<(max/3)) { // regra para que o cubo seja gerado no espaço de trabalho do braço robótico
+        if(choice == 1)
+        {
+            num = (((double)rand()/(double)RAND_MAX)*min); //gera número entre min e 0
+        }
+        else if(choice == 0)
+        {
+            num = (((double)rand()/(double)RAND_MAX)*max); // gera número entre 0 e max
+        }
     }
 
     return num;
@@ -285,35 +263,35 @@ double rand_number(double min, double max) {
 void FollowCurve(int orientation) {
 
     //caso Z=0
-    if(posZ == 0)
+    if(Zpos == 0)
     {
-        if(posX == -25)
+        if(Xpos == -25)
         {
-            posX+=1;
-            posY=15+2*sin(posX*(M_PI_4));
-            posZ=-sqrt(pow(25,2)-pow(posX,2));
+            Xpos+=1;
+            Ypos=15+2*sin(Xpos*(M_PI_4));
+            Zpos=-sqrt(pow(25,2)-pow(Xpos,2));
         }
-        else if(posX == 25)
+        else if(Xpos == 25)
         {
-            posX-=1;
-            posY=15+2*sin(posX*(M_PI_4));
-            posZ=sqrt(pow(25,2)-pow(posX,2));
+            Xpos-=1;
+            Ypos=15+2*sin(Xpos*(M_PI_4));
+            Zpos=sqrt(pow(25,2)-pow(Xpos,2));
         }
     }
 
     //caso Z>0
-    if(posX>-25 && posZ>0) {
-        posX-=1;
-        posY=15+2*sin(posX*(M_PI_4));
-        posZ=sqrt(pow(25,2)-pow(posX,2));
+    if(Xpos>-25 && Zpos>0) {
+        Xpos-=1;
+        Ypos=15+2*sin(Xpos*(M_PI_4));
+        Zpos=sqrt(pow(25,2)-pow(Xpos,2));
     }
 
     //caso Z<0
-    if(posX<25 && posZ<0)
+    if(Xpos<25 && Zpos<0)
     {
-        posX+=1;
-        posY=15+2*sin(posX*(M_PI_4));
-        posZ=-sqrt(pow(25,2)-pow(posX,2));
+        Xpos+=1;
+        Ypos=15+2*sin(Xpos*(M_PI_4));
+        Zpos=-sqrt(pow(25,2)-pow(Xpos,2));
     } 
 
 
@@ -325,47 +303,47 @@ void FollowCurve(int orientation) {
 
 /******************************************************************/
 
-void DrawCube(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength)
+void DrawCube(GLfloat centerXpos, GLfloat centerYpos, GLfloat centerZpos, GLfloat edgeLength)
 {
     GLfloat halfSideLength = edgeLength * 0.5;
     
     GLfloat vertices[] = {
 
         //face frontal
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // superior esquerdo
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // superior direito
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // inferior direito
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // inferior esquerdo
+        centerXpos - halfSideLength, centerYpos + halfSideLength, centerZpos + halfSideLength, // superior esquerdo
+        centerXpos + halfSideLength, centerYpos + halfSideLength, centerZpos + halfSideLength, // superior direito
+        centerXpos + halfSideLength, centerYpos - halfSideLength, centerZpos + halfSideLength, // inferior direito
+        centerXpos - halfSideLength, centerYpos - halfSideLength, centerZpos + halfSideLength, // inferior esquerdo
 
         //face distal
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // superior esquerdo
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // superior direito
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // inferior direito
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // inferior esquerdo
+        centerXpos - halfSideLength, centerYpos + halfSideLength, centerZpos - halfSideLength, // superior esquerdo
+        centerXpos + halfSideLength, centerYpos + halfSideLength, centerZpos - halfSideLength, // superior direito
+        centerXpos + halfSideLength, centerYpos - halfSideLength, centerZpos - halfSideLength, // inferior direito
+        centerXpos - halfSideLength, centerYpos - halfSideLength, centerZpos - halfSideLength, // inferior esquerdo
 
         //face lateral esquerda
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // superior esquerdo
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // superior direito
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // inferior direito
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // inferior esquerdo
+        centerXpos - halfSideLength, centerYpos + halfSideLength, centerZpos + halfSideLength, // superior esquerdo
+        centerXpos - halfSideLength, centerYpos + halfSideLength, centerZpos - halfSideLength, // superior direito
+        centerXpos - halfSideLength, centerYpos - halfSideLength, centerZpos - halfSideLength, // inferior direito
+        centerXpos - halfSideLength, centerYpos - halfSideLength, centerZpos + halfSideLength, // inferior esquerdo
 
         //face lateral direita
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // superior esquerdo
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // superior direito
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // inferior direito
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // inferior esquerdo
+        centerXpos + halfSideLength, centerYpos + halfSideLength, centerZpos + halfSideLength, // superior esquerdo
+        centerXpos + halfSideLength, centerYpos + halfSideLength, centerZpos - halfSideLength, // superior direito
+        centerXpos + halfSideLength, centerYpos - halfSideLength, centerZpos - halfSideLength, // inferior direito
+        centerXpos + halfSideLength, centerYpos - halfSideLength, centerZpos + halfSideLength, // inferior esquerdo
 
         //face superior
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // superior esquerdo
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // superior direito
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // inferior direito
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // inferior esquerdo
+        centerXpos - halfSideLength, centerYpos + halfSideLength, centerZpos + halfSideLength, // superior esquerdo
+        centerXpos - halfSideLength, centerYpos + halfSideLength, centerZpos - halfSideLength, // superior direito
+        centerXpos + halfSideLength, centerYpos + halfSideLength, centerZpos - halfSideLength, // inferior direito
+        centerXpos + halfSideLength, centerYpos + halfSideLength, centerZpos + halfSideLength, // inferior esquerdo
 
         //face inferior
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // superior esquerdo
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // superior direito
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // inferior direito
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength // inferior esquerdo
+        centerXpos - halfSideLength, centerYpos - halfSideLength, centerZpos + halfSideLength, // superior esquerdo
+        centerXpos - halfSideLength, centerYpos - halfSideLength, centerZpos - halfSideLength, // superior direito
+        centerXpos + halfSideLength, centerYpos - halfSideLength, centerZpos - halfSideLength, // inferior direito
+        centerXpos + halfSideLength, centerYpos - halfSideLength, centerZpos + halfSideLength // inferior esquerdo
     };
         glEnableClientState(GL_VERTEX_ARRAY);
             glVertexPointer(3, GL_FLOAT, 0, vertices);
@@ -380,27 +358,27 @@ void Mouse(int btn, int state, int x, int y) {
         case GLUT_LEFT_BUTTON:
             if(state == GLUT_DOWN) {
                 proj=1;
-                posX=0; posY=5; posZ=5;
-                oX=0; oY=0; oZ=0;
-                lX=0; lY=1; lZ=0;
+                Xpos=0; Ypos=5; Zpos=5;
+                Xo=0; Yo=0; Zo=0;
+                Xl=0; Yl=1; Zl=0;
                 glutPostRedisplay();
             }
             break;
         case GLUT_RIGHT_BUTTON:
             if(state == GLUT_DOWN) {
                 proj=0;
-                posX=0; posY=20; posZ=20;
-                oX=0; oY=0; oZ=0;
-                lX=0; lY=1; lZ=0;
+                Xpos=0; Ypos=20; Zpos=20;
+                Xo=0; Yo=0; Zo=0;
+                Xl=0; Yl=1; Zl=0;
                 glutPostRedisplay();
             }
             break;
         case GLUT_MIDDLE_BUTTON:
             if(state == GLUT_DOWN) {
                 proj=0;
-                posX=30; posY=0; posZ=0;
-                oX=0; oY=0; oZ=0;
-                lX=0; lY=1; lZ=0;
+                Xpos=30; Ypos=0; Zpos=0;
+                Xo=0; Yo=0; Zo=0;
+                Xl=0; Yl=1; Zl=0;
                 glutPostRedisplay();
             }
             break;
@@ -413,21 +391,21 @@ void Mouse(int btn, int state, int x, int y) {
 void Keyboard(unsigned char key, int x, int y) {
     //movimentações da câmera
     if(key=='d') {
-        oX+=5;
-        lY=1;
-        lZ=0;
+        Xo+=5;
+        Yl=1;
+        Zl=0;
     } else if(key=='a') {
-        oX-=5;
-        lY=1;
-        lZ=0;
+        Xo-=5;
+        Yl=1;
+        Zl=0;
     } else if(key=='w') {
-        oY+=5;
-        lY=1;
-        lZ=0;
+        Yo+=5;
+        Yl=1;
+        Zl=0;
     } else if(key=='s') {
-        oY-=5;
-        lY=1;
-        lZ=0;
+        Yo-=5;
+        Yl=1;
+        Zl=0;
     } else if(key=='r') {
         rot+=5;
     } else if(key=='R') {
@@ -436,9 +414,9 @@ void Keyboard(unsigned char key, int x, int y) {
         exit(0);
     } else if(key=='p') {
         proj=0;
-        posX=0, posY=15, posZ=25;
-        oX=0, oY=0, oZ=0;
-        lX=0, lY=1, lZ=0;
+        Xpos=0, Ypos=15, Zpos=25;
+        Xo=0, Yo=0, Zo=0;
+        Xl=0, Yl=1, Zl=0;
         rot=0;
     } 
     else if(key=='g'){ //tecla g é utilizada para iniciar a trajetória
@@ -448,79 +426,77 @@ void Keyboard(unsigned char key, int x, int y) {
     
     // primeira esfera
     else if(key=='u') {
-        angulo1 = (angulo1 - 5) % 360;
+        angle1 = (angle1 - 5) % 360;
     }
     else if(key=='U') {
-        angulo1 = (angulo1 + 5) % 360;
+        angle1 = (angle1 + 5) % 360;
     }
         else if(key=='i') {
-        angulo2 = (angulo2 - 5) % 360;
+        angle2 = (angle2 - 5) % 360;
     }
     else if(key=='I') {
-        angulo2 = (angulo2 + 5) % 360;
+        angle2 = (angle2 + 5) % 360;
     }
         else if(key=='o') {
-        angulo3 = (angulo3 - 5) % 360;
+        angle3 = (angle3 - 5) % 360;
     }
     else if(key=='O') {
-        angulo3 = (angulo3 + 5) % 360;
+        angle3 = (angle3 + 5) % 360;
     }
 
 
     //segunda esfera
     else if(key=='j') {
-        angulo4 = (angulo4 - 5) % 360;
+        angle4 = (angle4 - 5) % 360;
     }
     else if(key=='J') {
-        angulo4 = (angulo4 + 5) % 360;
+        angle4 = (angle4 + 5) % 360;
     }
     else if(key=='k') {
-        angulo5 = (angulo5 - 5) % 360;
+        angle5 = (angle5 - 5) % 360;
     }
     else if(key=='K') {
-        angulo5 = (angulo5 + 5) % 360;
+        angle5 = (angle5 + 5) % 360;
     }
        else if(key=='l') {
-        angulo6 = (angulo6 - 5) % 360;
+        angle6 = (angle6 - 5) % 360;
     }
     else if(key=='L') {
-        angulo6 = (angulo6 + 5) % 360;
+        angle6 = (angle6 + 5) % 360;
     }
 
 
     //terceira esfera
     else if(key=='b') {
-        garra.ang_x = (garra.ang_x - 5) % 360;
+        angle7 = (angle7 - 5) % 360;
     }
     else if(key=='B') {
-        garra.ang_x = (garra.ang_x + 5) % 360;
+        angle7 = (angle7 + 5) % 360;
     }
        else if(key=='n') {
-        garra.ang_y = (garra.ang_y - 5) % 360;
+        angle8 = (angle8 - 5) % 360;
     }
     else if(key=='N') {
-        garra.ang_y = (garra.ang_y + 5) % 360;
+        angle8 = (angle8 + 5) % 360;
     }
        else if(key=='m') {
-        garra.ang_z = (garra.ang_z - 5) % 360;
+        angle9 = (angle9 - 5) % 360;
     }
     else if(key=='M') {
-        garra.ang_z = (garra.ang_z + 5) % 360;
+        angle9 = (angle9 + 5) % 360;
     }
 
     //movimento linear da garra
     else if(key=='+') {
-        grab = 0;
-           if(dedo_dir.pos_z - dedo_esq.pos_z <= 0.1) {
-            dedo_esq.pos_z -= 0.01;
-            dedo_dir.pos_z += 0.01;
+           if(pos6 - pos3 <= 0.1) {
+            pos3 -= 0.01;
+            pos6 += 0.01;
         }
     }
     else if(key=='-') {
-        grab = 0;
-        if((dedo_dir.pos_z - dedo_esq.pos_z)>0) {
-            dedo_dir.pos_z -= 0.01;
-            dedo_esq.pos_z += 0.01;
+        if((pos6 - pos3)>0) {
+            pos6 -= 0.01;
+            pos3 += 0.01;
         }
     }
 
@@ -533,23 +509,23 @@ void Keyboard(unsigned char key, int x, int y) {
 
 void SpecKeys(int key, int x, int y) {
     if(key==GLUT_KEY_RIGHT) {
-        posX+=5;
-        oX+=5;
+        Xpos+=5;
+        Xo+=5;
     } else if (key==GLUT_KEY_LEFT) {
-        posX-=5;
-        oX-=5;
+        Xpos-=5;
+        Xo-=5;
     } else if (key==GLUT_KEY_UP) {
-        posY+=5;
-        oY+=5;
+        Ypos+=5;
+        Yo+=5;
     } else if (key==GLUT_KEY_DOWN) {
-        posY-=5;
-        oY-=5;
+        Ypos-=5;
+        Yo-=5;
     } else if (key==GLUT_KEY_PAGE_UP) {
-        posZ+=5;
-        oZ+=5;
+        Zpos+=5;
+        Zo+=5;
     } else if (key==GLUT_KEY_PAGE_DOWN) {
-        posZ-=5;
-        oZ-=5;
+        Zpos-=5;
+        Zo-=5;
     }
     glutPostRedisplay();
 }
